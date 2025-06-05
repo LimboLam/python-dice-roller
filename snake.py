e2ee8f3d746e05b767267b88d171c21b72c690dc
LimboLam <liam.xiong24@student.cart.org>
2025-06-05 10:34:47 -0700

    arrows keys :D

--- snake.py
+++ snake.py
@@ -68,26 +68,30 @@ def main():
                 running = False
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
+                if event.key in [pygame.K_a, pygame.K_LEFT]:
                     if direction == 'right':
                         direction = 'right'
                     else:
                         direction = 'left'
-                elif event.key == pygame.K_w:
+                    print('Left')
+                elif event.key in [pygame.K_UP, pygame.K_w]:
                     if direction == 'down':
                         direction = 'down'
                     else:
                         direction = 'up'
-                elif event.key == pygame.K_s:
+                    print('Up')
+                elif event.key in [pygame.K_DOWN, pygame.K_s]:
                     if direction == 'up':
                         direction = 'up'
                     else:
                         direction = 'down'
-                elif event.key == pygame.K_d:
+                    print('Down')
+                elif event.key in [pygame.K_RIGHT, pygame.K_d]:
                     if direction == 'left':
                         direction = 'left'
                     else:
                         direction = 'right'
+                    print('Right')
 
             if start:
                 if event.type == pygame.KEYDOWN:
